ba6442e6bbd21d34650980c48bc688a5a9ae9406
Thomas Hofmann <thofmann2104@gmail.com>
2021-10-28 21:49:58 +0200

    Running Springboot App

--- IntelliJ/Spring-Boot-Tutorial/src/main/java/com/dailycodebuffer/SpringBoot/Tutorial/controller/HelloController.java
+++ IntelliJ/Spring-Boot-Tutorial/src/main/java/com/dailycodebuffer/SpringBoot/Tutorial/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.dailycodebuffer.SpringBoot.Tutorial.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    //@RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping("/") // kann so direkt verwendet werden
     public String helloWorld() {
         return "Welcome to Daily Code Buffer";
     }
