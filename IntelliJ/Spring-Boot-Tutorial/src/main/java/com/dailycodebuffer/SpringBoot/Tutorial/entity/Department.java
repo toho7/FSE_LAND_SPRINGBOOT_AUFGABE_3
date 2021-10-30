e67a399ed686e6bd5c301be2b3afddcbb277ab40
Thomas Hofmann <thofmann2104@gmail.com>
2021-10-30 13:55:52 +0200

    Project Lombok (Removing Boiler plate code)

--- IntelliJ/Spring-Boot-Tutorial/src/main/java/com/dailycodebuffer/SpringBoot/Tutorial/entity/Department.java
+++ IntelliJ/Spring-Boot-Tutorial/src/main/java/com/dailycodebuffer/SpringBoot/Tutorial/entity/Department.java
@@ -1,6 +1,6 @@
 package com.dailycodebuffer.SpringBoot.Tutorial.entity;
 
-import org.hibernate.validator.constraints.Length;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +9,10 @@ import javax.persistence.Id;
 import javax.validation.constraints.*;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Department {
 
     // Properties
@@ -28,57 +32,4 @@ public class Department {
     private String departmentAddress;
     private String departmentCode;
 
-    // Constructor
-    public Department(Long departmentID, String departmentName, String departmentAddress, String departmentCode) {
-        this.departmentID = departmentID;
-        this.departmentName = departmentName;
-        this.departmentAddress = departmentAddress;
-        this.departmentCode = departmentCode;
-    }
-
-    public Department() {
-    }
-
-    // Getter und Setter und toString
-    public Long getDepartmentID() {
-        return departmentID;
-    }
-
-    public void setDepartmentID(Long departmentID) {
-        this.departmentID = departmentID;
-    }
-
-    public String getDepartmentName() {
-        return departmentName;
-    }
-
-    public void setDepartmentName(String departmentName) {
-        this.departmentName = departmentName;
-    }
-
-    public String getDepartmentAddress() {
-        return departmentAddress;
-    }
-
-    public void setDepartmentAddress(String departmentAddress) {
-        this.departmentAddress = departmentAddress;
-    }
-
-    public String getDepartmentCode() {
-        return departmentCode;
-    }
-
-    public void setDepartmentCode(String departmentCode) {
-        this.departmentCode = departmentCode;
-    }
-
-    @Override
-    public String toString() {
-        return "Department{" +
-                "departmentID=" + departmentID +
-                ", departmentName='" + departmentName + '\'' +
-                ", departmentAddress='" + departmentAddress + '\'' +
-                ", departmentCode='" + departmentCode + '\'' +
-                '}';
-    }
 }
