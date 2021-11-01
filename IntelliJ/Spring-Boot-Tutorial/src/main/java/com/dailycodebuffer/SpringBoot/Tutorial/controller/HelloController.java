52acb0d2194aad44bf3f7e06f3bb4af7b02e550d
Thomas Hofmann <thofmann2104@gmail.com>
2021-11-01 23:16:54 +0100

    Adding Config In Properties File

--- IntelliJ/Spring-Boot-Tutorial/src/main/java/com/dailycodebuffer/SpringBoot/Tutorial/controller/HelloController.java
+++ IntelliJ/Spring-Boot-Tutorial/src/main/java/com/dailycodebuffer/SpringBoot/Tutorial/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.dailycodebuffer.SpringBoot.Tutorial.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,9 +9,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    // Auskommentiert für Adding Config in Properties File
+    /*
     //@RequestMapping(value = "/", method = RequestMethod.GET)
     @GetMapping("/") // kann so direkt verwendet werden
     public String helloWorld() {
         return "Welcome to Daily Code Buffer.";
     }
+    */
+
+    // Adding Config in Properties File
+
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return welcomeMessage;
+    }
+
 }
